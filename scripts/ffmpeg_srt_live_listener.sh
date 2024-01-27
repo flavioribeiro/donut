809b5ea195827e2b363a30e8ef6a87ae2f1a91a2
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2024-01-27 10:01:28 -0300

    uses mpeg1 for audio

--- scripts/ffmpeg_srt_live_listener.sh
+++ scripts/ffmpeg_srt_live_listener.sh
@@ -2,7 +2,6 @@ ffmpeg -hide_banner -loglevel verbose \
     -re -f lavfi -i "testsrc2=size=1280x720:rate=30,format=yuv420p" \
     -f lavfi -i "sine=frequency=1000:sample_rate=44100" \
     -c:v libx264 -preset veryfast -tune zerolatency -profile:v baseline \
-    -b:v 1400k -bufsize 2800k -x264opts keyint=30:min-keyint=30:scenecut=-1 \
-    -c:a aac -b:a 128k \
+    -b:v 1000k -bufsize 2000k -x264opts keyint=30:min-keyint=30:scenecut=-1 \
     -f mpegts "srt://${SRT_LISTENING_HOST}:${SRT_LISTENING_PORT}?mode=listener&latency=${SRT_LISTENING_LATENCY_US}"
 
