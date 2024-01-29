1812137aefa204b9f4ae76ec94b741cd484c5564
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2024-01-29 09:29:40 -0300

    set live mode for srt

--- scripts/srt.sh
+++ scripts/srt.sh
@@ -1,4 +1,4 @@
 # ref https://github.com/Haivision/srt/blob/master/docs/apps/srt-live-transmit.md
 srt-live-transmit \
-    udp://${SRT_UDP_TS_INPUT_HOST}:${SRT_UDP_TS_INPUT_PORT} \
-    srt://:${SRT_LISTENING_PORT}?congestion=live -v
+    "udp://${SRT_UDP_TS_INPUT_HOST}:${SRT_UDP_TS_INPUT_PORT}" \
+    "srt://:${SRT_LISTENING_PORT}?congestion=live&transtype=live&groupconnect=1" -v
